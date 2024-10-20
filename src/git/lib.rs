4c0d7e5f60cd39c4fda1264c55914f826fd761e2
ppaprikaa <sanzhar.maratov@proton.me>
2024-10-20 06:08:28 +0500

    move: git_lib module as git submodule

--- src/git/lib.rs
+++ src/git/lib.rs
@@ -1,4 +1,2 @@
-use std::error::Error;
-
 pub fn cat_file(pretty: bool, object: String) { 
 }
--- src/git/mod.rs
+++ src/git/mod.rs
@@ -1,6 +1,8 @@
 use crate::cli_parser::{Query, Commands};
 use crate::git_lib::cat_file;
 
+mod lib;
+
 pub fn run(query: Query) -> Result<(), String> {
     match query.command {
         Commands::CatFile { pretty, object }  => {
