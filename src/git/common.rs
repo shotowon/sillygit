fa1802b4ac612ac4b49227222158d6e9d94831a9
ppaprikaa <sanzhar.maratov@proton.me>
2024-10-20 18:46:21 +0500

    added: error type

--- src/git/common.rs
+++ src/git/common.rs
@@ -1,9 +1,33 @@
 use flate2::read::ZlibDecoder;
-use std::io::prelude::*;
-use std::fs::File;
-use std::error::Error;
+use std::{
+    io::prelude::*,
+    fs::File,
+    error,
+    fmt,
+};
 
-pub fn decode_from_file(file: &File) -> Result<String, Box<dyn Error>> {
+#[derive(Debug)]
+pub struct Error {
+    details: String,
+}
+
+impl Error {
+    fn new(details: String) -> Self {
+        Error {
+            details: details,
+        }
+    }
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.details)
+    }
+}
+
+impl error::Error for Error {}
+
+pub fn decode_from_file(file: &File) -> Result<String, Box<dyn error::Error>> {
     let mut d = ZlibDecoder::new(file);
     let mut buf = String::new();
     d.read_to_string(&mut buf)?;
