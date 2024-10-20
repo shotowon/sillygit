bf742dd79effdbded2fb29fe10744d4cebf64ae6
ppaprikaa <sanzhar.maratov@proton.me>
2024-10-20 06:18:32 +0500

    implemented: init command

--- src/git/lib.rs
+++ src/git/lib.rs
@@ -1,2 +1,14 @@
+use std::fs;
+use std::io::Error;
+
 pub fn cat_file(pretty: bool, object: String) { 
+}
+
+pub fn init() -> Result<(), Error> {
+    fs::create_dir(".tinygit")?;
+    fs::create_dir(".tinygit/objects")?;
+    fs::create_dir(".tinygit/refs")?;
+    fs::write(".tinygit/HEAD", "ref: refs/heads/main\n")?;
+    println!("Initialized git directory");
+    Ok(())
 }
--- src/git/mod.rs
+++ src/git/mod.rs
@@ -1,14 +1,15 @@
 use crate::cli_parser::{Query, Commands};
 
+use std::io::Error;
+
 mod lib;
 
-pub fn run(query: Query) -> Result<(), String> {
+pub fn run(query: Query) -> Result<(), Error> {
     match query.command {
         Commands::CatFile { pretty, object }  => {
             lib::cat_file(pretty, object);
         },
-        Commands::Init => {
-        },
+        Commands::Init => lib::init()?,
     }
 
     Ok(())
