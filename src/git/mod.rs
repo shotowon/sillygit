a4f56fac4eaf6fde3741d4b1ad7387e63793770a
ppaprikaa <sanzhar.maratov@proton.me>
2024-10-21 00:15:21 +0500

    added: git objects handling type

--- src/git/mod.rs
+++ src/git/mod.rs
@@ -5,6 +5,7 @@ use std::error::Error;
 mod lib;
 mod common;
 mod consts;
+mod object;
 
 pub fn run(query: Query) -> Result<(), Box<dyn Error>> {
     match query.command {
---
+++ src/git/object.rs
@@ -0,0 +1,45 @@
+use std::{
+    fs::File,
+    path::Path,
+    error,
+};
+
+use crate::git::consts::REPO_DIRECTORY;
+use crate::git::common::{Error, decode_from_file};
+
+pub enum ObjectFile {
+    Blob {
+        name: String,
+        header: String,
+        content: String,
+    },
+}
+
+impl ObjectFile {
+    pub fn new(object: &str) -> Result<Self, Box<dyn error::Error>> {
+            let filepath = &format!(
+                "{}/objects/{}/{}",
+                REPO_DIRECTORY,
+                &object[..2],
+                &object[2..]
+            );
+
+            let file = Path::new(&filepath);
+            let file = File::open(&file)?;
+            let contents = decode_from_file(&file)?;
+
+            if contents.starts_with("blob") {
+                let contents: Vec<&str> = contents.split('\0').collect();
+                let header = contents[0];
+                return Ok(
+                    ObjectFile::Blob {
+                        name: object.to_string(),
+                        header: header.to_string(), 
+                        content: contents[1].to_string(),
+                    }
+                )
+            }
+            
+            Err(Box::new(Error::new("failed to identify object")))
+    }
+}
