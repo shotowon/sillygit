28b0b643f7628a7c613e5fd36faf170832ddc090
ppaprikaa <sanzhar.maratov@proton.me>
2024-10-20 05:21:55 +0500

    added: parse cat-file -p

---
+++ src/cli_parser/mod.rs
@@ -0,0 +1,21 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+pub struct Query {
+    #[command(subcommand)]
+    pub command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+pub enum Commands {
+    CatFile {
+        // Pretty print of the contents.
+        #[arg(short, long)]
+        pretty: bool,
+
+        // object
+        #[arg(required = true)]
+        object: String,
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,9 @@
+#![allow(unused)]
+extern crate tinygit;
+use clap::Parser;
+use tinygit::cli_parser::Query;
+
+
 fn main() {
+    let query = Query::parse();
 }
